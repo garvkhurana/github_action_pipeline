a6d670fd4502534aa1d9b7f50050de101847424e
garvkhurana <garvkhurana1234567@gmail.com>
2025-02-01 19:21:14 +0530

    commit

--- test_app.py
+++ test_app.py
@@ -1,7 +1,8 @@
 from app import app
 
-def test_home():  #3if we want pytest library to act we have to make functions and files starting name with test 
-    response=app.test_client().get('/')
+def test_home():  
+    """Test the home route."""
+    response = app.test_client().get('/')
     
-    assert response.status_code==200
-    assert response.data=="Hello world"
+    assert response.status_code == 200
+    assert response.data == b"hello world"  # Convert expected output to bytes
